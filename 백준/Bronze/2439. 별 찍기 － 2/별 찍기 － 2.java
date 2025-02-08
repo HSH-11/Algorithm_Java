2a30ff482e6128d7b4482513d05ad7ce2c931f68
HSH-11 <lyk1517@naver.com>
2025-02-09 01:33:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 208 ms, Memory: 19784 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N - i - 1; j++) {
 					System.out.print(" ");
 			}
-			for (int j = N; j > N-i-1; j--) {
+			for (int j = N; j > N - i - 1; j--) {
 					System.out.print("*");
 			}
 		System.out.println();	
