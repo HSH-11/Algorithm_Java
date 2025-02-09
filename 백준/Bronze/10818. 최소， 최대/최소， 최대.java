d2e32dac7ecf1eca66695b1c10c0b0d31fb372a5
HSH-11 <lyk1517@naver.com>
2025-02-09 15:29:18 +0900

    [Bronze III] Title: 최소, 최대, Time: 480 ms, Memory: 88176 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,15 +8,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			if (num < min) {min = num;}
+			if (num > max) {max = num;}
 		}
-
-		int max = Arrays.stream(arr).max().getAsInt();
-		int min = Arrays.stream(arr).min().getAsInt();
 		
 		System.out.println(min+" "+max);
 		
