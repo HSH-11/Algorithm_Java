0480c1338c3725c4926d6d7a4a4a849927ff15f8
HSH-11 <lyk1517@naver.com>
2025-02-09 13:54:52 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -8,15 +8,18 @@ public class Main {
 		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
+		StringBuilder sb = new StringBuilder();
+		
 		int n = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j <= i; j++) {
-				System.out.print("*");
+				sb.append("*");
 			}
-			System.out.println();
+			sb.append('\n');
 		}
+		System.out.print(sb);
+		br.close();
 			
 	}
 	
