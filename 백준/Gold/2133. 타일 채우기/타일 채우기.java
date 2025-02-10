0ba276e44082f5c0ffb901e09d5035f8c711270d
HSH-11 <lyk1517@naver.com>
2025-02-11 00:16:19 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N =  Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
+		if (N % 2 == 1) {
+			System.out.println("0");
+			return;
+		}
+        dp[0] = 1;
+		dp[2] = 3;
+		for (int i = 4; i <= N; i += 2) {
+			dp[i] = dp[i-2]*3;
+			for (int j = 4; j<=i ; j+=2) {
+				dp[i] += dp[i-j]*2;
+			}
+			
+		}
+		System.out.println(dp[N]);
+	}
+}
