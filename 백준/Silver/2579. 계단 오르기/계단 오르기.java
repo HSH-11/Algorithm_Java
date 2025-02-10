8b4cd943158725400b5120e80c58bb7a3decbdeb
HSH-11 <lyk1517@naver.com>
2025-02-10 22:16:16 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr =  new int[N+1];
+		int[] dp = new int[N+1];
+				
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		if (N == 1) {
+			System.out.println(arr[1]);
+			return;
+		}
+		
+		dp[1]= arr[1];
+		if (N >= 2) {
+			dp[2] = arr[1]+arr[2];
+		}
+		if (N >= 3) {
+			dp[3] = Math.max(arr[1]+arr[3], arr[2]+arr[3]);
+		}
+		
+		for (int i = 4; i <= N; i++) {
+			dp[i] = Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]);
+		}
+		System.out.println(dp[N]);
+	
+	}
+}
