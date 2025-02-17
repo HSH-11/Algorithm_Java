e5526134b3419f0cee14fd8ff557d2a04ecccdcd
HSH-11 <lyk1517@naver.com>
2025-02-17 15:55:03 +0900

    [Gold II] Title: 빵집, Time: 344 ms, Memory: 34356 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static char[][] map;
+	//오른쪽,오른쪽 위,오른쪽 아래
+	static int[] dy = {-1,0,1}; 
+	static int R,C,count;
+	
+
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    R = Integer.parseInt(st.nextToken());
+	    C = Integer.parseInt(st.nextToken());
+	    map = new char[R][C];
+
+	    for (int i = 0; i < R; i++) {
+	        String line = br.readLine();
+	        for (int j = 0; j < C; j++) {
+	            	map[i][j] = line.charAt(j);               
+	        }
+	    }
+	    
+	    count = 0;
+	    for (int i = 0; i < R; i++) {
+	    	if(dfs(i, 0)) {
+	    		count++;
+	    	}
+	    }
+	    
+	    System.out.println(count);
+	       
+	}
+
+	static boolean dfs(int y, int x) {
+	    
+		if (x == C - 1) return true; 
+
+	    for (int i = 0; i < 3; i++) {
+	    	int ny = y + dy[i];
+	    	int nx = x + 1;
+	    	
+	    	if (ny >= 0 && ny < R && nx < C && map[ny][nx] == '.') {
+	    		map[ny][nx] = 'x';
+	    		if(dfs(ny, nx)) {
+	    			return true;
+	    		}
+	    	}
+	    		
+	    	
+	    }
+	    return false;
+	}
+
+}
