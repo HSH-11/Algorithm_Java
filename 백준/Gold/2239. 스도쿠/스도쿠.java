20c31318115be8f3a809e7b5b4db546745686ded
HSH-11 <lyk1517@naver.com>
2025-02-17 14:06:49 +0900

    [Gold IV] Title: 스도쿠, Time: 776 ms, Memory: 15780 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	static int[][] board;
-
+	static List<Block> emptyCells = new ArrayList<>();
+	
 	static class Block {
 		int y, x;
 
@@ -17,62 +19,53 @@ public class Main {
 
 	}
 
+	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		board = new int[9][9];
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    board = new int[9][9];
 
-		for (int i = 0; i < 9; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < 9; j++) {
-				board[i][j] = line.charAt(j) - '0';
-			}
-		}
+	    for (int i = 0; i < 9; i++) {
+	        String line = br.readLine();
+	        for (int j = 0; j < 9; j++) {
+	            board[i][j] = line.charAt(j) - '0';
+	            if (board[i][j] == 0) {
+	                emptyCells.add(new Block(i, j)); 
+	            }
+	        }
+	    }
 
-		findSolution();
+	    findSolution(0);
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				sb.append(board[i][j]);
-			}
-			sb.append("\n");
-		}
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < 9; i++) {
+	        for (int j = 0; j < 9; j++) {
+	            sb.append(board[i][j]);
+	        }
+	        sb.append("\n");
+	    }
 
-		System.out.println(sb);
-
+	    System.out.println(sb);
 	}
 
-	static Block findEmpty() {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] == 0) {
-					return new Block(i, j);
-				}
-			}
-		}
-		return null;
+	static boolean findSolution(int idx) {
+	    if (idx == emptyCells.size()) return true; 
+
+	    Block pos = emptyCells.get(idx);
+	    int row = pos.y, col = pos.x;
+
+	    for (int num = 1; num <= 9; num++) {
+	        if (is_valid(num, row, col)) {
+	            board[row][col] = num;
+	            if (findSolution(idx + 1)) {
+	                return true;
+	            }
+	            board[row][col] = 0;
+	        }
+	    }
+	    return false;
 	}
 
-	static boolean findSolution() {
-		Block pos = findEmpty();
-		
-		if (pos == null)
-			return true;
-
-		int row = pos.y;
-		int col = pos.x;
-
-		for (int num = 1; num <= 9; num++) {
-			if (is_valid(num, row, col)) {
-				board[row][col] = num;
-				if (findSolution()) {
-					return true;
-				}
-				board[row][col] = 0;
-			}
-		}
-		return false;
-	}
 
 	static boolean is_valid(int num, int row, int col) {
 		int boxRow = (row / 3) * 3;
