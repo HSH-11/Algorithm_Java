5a3ddd175141ee913ee40aea6be30506698a9265
HSH-11 <lyk1517@naver.com>
2025-02-18 17:44:33 +0900

    [Silver I] Title: 그림, Time: 360 ms, Memory: 51852 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,74 +1,70 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] paper;
+	static int N, M;
+	static int map[][];
 	static boolean[][] visited;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static int N,M;
-	
-	public static void main(String[] args) throws IOException {
+	static int max_count, max_area;
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { -1, 1, 0, 0 };
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
-		paper = new int[N][M];
+
+		map = new int[N][M];
 		visited = new boolean[N][M];
-		
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				paper[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		int count = 0;
-		int max = 0;
-		
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (paper[i][j] == 1 && !visited[i][j]) {
-					count++;
-					max = Math.max(max, bfs(i,j));
+				if (map[i][j] == 1 && !visited[i][j]) {
+					max_count++;
+					max_area = Math.max(max_area, bfs(i, j));
 				}
 			}
 		}
-		
-		System.out.println(count);
-		System.out.println(max);
-					
-	}
-	
-	static int bfs(int s, int e) {
-		Queue<int[]> queue = new ArrayDeque<int[]>();
-		queue.add(new int[] {s,e});
-		visited[s][e] = true;
-		
-		int area = 1;
-		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int cx = cur[0];
-			int cy = cur[1];
-			
-			for (int i = 0; i < 4; i++) {			
-					int nx = cx + dx[i];
-					int ny = cy + dy[i];
-					
-					if (nx >=0 && nx < N && ny >= 0 && ny < M) {
-						if (!visited[nx][ny] && paper[nx][ny] == 1) {
-							visited[nx][ny] = true;
-							area++;
-							queue.add(new int[] {nx,ny});
-						}
-					}
-					
-				
-			}
-		}
-		return area;
+
+		System.out.println(max_count);
+		System.out.println(max_area);
 	}
 
+	public static int bfs(int y, int x) {
+		Queue<int[]> queue = new ArrayDeque<int[]>();
+		queue.add(new int[] { y, x });
+		visited[y][x] = true;
+
+		int area = 1;
+
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nx = curr[1] + dx[i];
+				int ny = curr[0] + dy[i];
+
+				if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+					if (!visited[ny][nx] && map[ny][nx] == 1) {
+						area++;
+						visited[ny][nx] = true;
+						queue.add(new int[] { ny, nx });
+					}
+				}
+			}
+		}
+
+		return area;
+	}
 }
