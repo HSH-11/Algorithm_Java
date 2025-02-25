8cc8c7dfcabd47aab1a102d5ef5f0bb3a430dee8
HSH-11 <lyk1517@naver.com>
2025-02-25 18:07:51 +0900

    [Gold IV] Title: LCS 2, Time: 144 ms, Memory: 18768 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String A = br.readLine();
+		String B = br.readLine();
+		
+		int n = A.length();
+		int m = B.length();
+		
+		dp = new int[n+1][m+1];
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if(A.charAt(i-1) == B.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		
+		System.out.println(dp[n][m]);
+		
+		StringBuilder sb = new StringBuilder();
+		int i = n;
+		int j = m;
+		
+		while(i > 0  && j > 0 ) {
+			if (A.charAt(i-1) == B.charAt(j-1)) {
+				sb.append(A.charAt(i-1));
+				i--;
+				j--;
+			}else if(dp[i-1][j] >= dp[i][j-1]){
+				i--;
+			}else {
+				j--;
+			}
+		}
+		
+		System.out.println(sb.reverse().toString());
+
+	}
+
+}
