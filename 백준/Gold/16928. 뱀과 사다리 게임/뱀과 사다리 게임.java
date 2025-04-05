31a7516e6865a30fc6e7d7ed2468c0bffe6d31fd
HSH-11 <lyk1517@naver.com>
2025-04-05 17:44:12 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 112 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+// 문제 조건
+// 게임의 크기는 정해짐 (10 * 10)
+// 보드판에는 1부터 100까지의 수가 하나씩 순서대로 적힘
+// 도착한 칸이 사다리면, 사다리를 타고 위로 올라간다.(사다리로 이동한 칸의 번호 > 원래 있던 칸 번호)
+// 뱀이 있는 칸에 도착하면, 뱀을 따라서 내려가게 된다.
+// 1번칸에서 시작해서 100번 칸에 도착하는 데 100번 칸에 도착하기 위해 주사위를 굴려야하는 최솟값을 구해야한다.
+
+// 문제 해결
+// BFS 방식(큐는 현재 위치 및 이동 횟수 정보를 가짐)
+// 사다리와 뱀 위치를 Map으로 저장한다.
+// 주사위를 굴리면서 100을 넘어가는 경우는 큐에서 제외
+// 현재 위치를 기준으로 주사위를 굴려 다음에 이동할 위치에 사다리 혹은 뱀이 있는 경우는 이동 위치 새로 갱신 
+
+public class Main {
+
+	static int N, M;
+	static HashMap<Integer, Integer> ladder_snake;
+	static int[] board = new int[101]; // 이동 횟수를 저장함
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken()); // 사다리 수
+		M = Integer.parseInt(st.nextToken()); // 뱀의 수
+		
+		// board
+		Arrays.fill(board, Integer.MAX_VALUE);
+		// 사다리 뱀 위치
+		ladder_snake = new HashMap<Integer, Integer>();
+		for (int i = 0; i < N + M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int curr = Integer.parseInt(st.nextToken());
+			int next = Integer.parseInt(st.nextToken());
+
+			ladder_snake.put(curr, next);
+		}
+
+		bfs();
+
+		System.out.println(board[100]);
+	}
+
+	static void bfs() {
+		Deque<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] { 1, 0 }); // 위치와 현재까지의 이동 횟수
+
+		while (!queue.isEmpty()) {
+			int[] info = queue.poll();
+			int pos = info[0];
+			int move = info[1];
+
+			for (int i = 1; i <= 6; i++) {
+				int next_pos = pos + i;
+				if (next_pos > 100)
+					continue;
+				if (ladder_snake.containsKey(next_pos)) {
+					next_pos = ladder_snake.get(next_pos);
+				}
+				int next_move = move + 1;
+
+				if (board[next_pos] > next_move) {
+					board[next_pos] = next_move;
+					queue.add(new int[] { next_pos, next_move });
+				}
+
+			}
+		}
+	}
+}
