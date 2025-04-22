ee4fcb1e819f37e285be113b36c5a812022f5d3d
HSH-11 <lyk1517@naver.com>
2025-04-22 22:10:49 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 288 ms, Memory: 23664 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -19,6 +19,8 @@ import java.util.StringTokenizer;
 // 처음 위치에 올릴 수 있으면 올림
 // 종료: 내구도 0의 칸 K개 이상
 
+//언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 22596 KB , 시간 : 392ms
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -28,57 +30,64 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int[] belt = new int[N * 2];
-		boolean[] robots = new boolean[N];
+		int[] belt = new int[N * 2 + 1];
+		boolean[] robots = new boolean[N + 1];
 
 		st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < N * 2; i++) {
+		for (int i = 1; i <= N * 2; i++) {
 			belt[i] = Integer.parseInt(st.nextToken());
 		}
 
 		int step = 1;
-		int start = 0; // 회전 시작 인덱스
-
+		
 		// 로봇을 옮기는 과정
 		while (true) {
 			// 1. 벨트 + 로봇 회전
-			start = (start - 1 + 2 * N) % (2 * N); // 한 칸 왼쪽으로 회전
-
+			int temp = belt[N*2];
 			// 로봇도 함께 회전
-			for (int i = N - 1; i > 0; i--) {
-				robots[i] = robots[i - 1];
-			}
-			robots[0] = false;
-			robots[N - 1] = false; // 내리는 위치에 로봇 제거
-
-			// 2. 로봇 이동
-			for (int i = N - 2; i >= 0; i--) {
-				if (robots[i]) {
-					int nextIdx = (start + i + 1) % (2 * N);
-
-					if (!robots[i + 1] && belt[nextIdx] > 0) {
-						belt[nextIdx]--;
-						robots[i + 1] = true;
-						robots[i] = false;
-					}
-				}
-
+			for (int i = N * 2; i > 1; i--) {
+				belt[i] = belt[i - 1];
 			}
 			
-			robots[N-1] = false; // 내리는 위치
+			belt[1] = temp;
+			
+			//로봇의 위치를 한칸씩 옮긴다.
+            for (int i = N; i > 1; i--) {
+                robots[i] = robots[i-1];
+            }
+
+            robots[1] = false;
+
+            //내리는 위치에 로봇이 있으면 내린다.
+            if(robots[N]) {
+                robots[N] = false;
+            }
+			// 2. 로봇 이동
+			for (int i = N - 1; i > 0; i--) {
+				if (robots[i] && !robots[i+1] && belt[i+1] >= 1) {
+                    belt[i+1]--;
+                    robots[i] = false;
+                    robots[i+1] = true;
+                }
+            }
+
+            //내리는 위치에 로봇이 있으면 내린다.
+            if(robots[N]) {
+                robots[N] = false;
+            }
 
 
 			// 3번: 올리는 위치에 있는 칸의 내구도가 0이 아니면 올리는 위치에 로봇을 올린다.
-			if (belt[start] > 0) {
-				robots[0] = true;
-				belt[start]--;
+			if (belt[1] > 0) {
+				robots[1] = true;
+				belt[1]--;
 			}
 
 			// 4번: 내구도가 0인 칸의 개수가 K개 이상이라면 과정을 종료한다. 그렇지 않다면 1번으로 돌아간다.
 			 int zeroCount = 0;
-	            for (int durability : belt) {
-	                if (durability == 0) zeroCount++;
+	            for (int i = 1; i<= N * 2; i++) {
+	                if (belt[i] == 0) zeroCount++;
 	            }
 	            if (zeroCount >= K) {
 	                System.out.println(step);
