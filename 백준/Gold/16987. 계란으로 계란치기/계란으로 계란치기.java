29ea02b23272941b200dedb3b6c1b5ae1724cf83
HSH-11 <lyk1517@naver.com>
2025-04-26 15:46:58 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 196 ms, Memory: 16788 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -14,68 +14,69 @@ public class Main {
 
 	static Egg[] eggs;
 	static int N;
-	static int maxBroken;
+	static int maxBroken = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		N = Integer.parseInt(br.readLine());
 		eggs = new Egg[N];
 
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-
-			eggs[i] = new Egg(d, w);
+			int durability = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			eggs[i] = new Egg(durability, weight);
 		}
 
-		dfs(0);
-		
+		dfs(0, 0);
+
 		System.out.println(maxBroken);
-
 	}
-	
-	static void dfs(int idx) {
-		// 모든 계란을 다 든 경우
-		if (idx == N) {
-			int broken = 0;
-			for (int i = 0; i < N; i++) {
-				if (eggs[i].durability <= 0) broken++;
-			}
-			maxBroken = Math.max(maxBroken, broken);
+
+	static void dfs(int idx, int brokenCount) {
+		// 현재 깨진 계란 수가 이미 최대면 더 탐색할 필요 없음
+		if (brokenCount == N || idx == N) {
+			maxBroken = Math.max(maxBroken, brokenCount);
 			return;
 		}
-		
-		
-		
-		// 현재 계란이 깨져버리면 다음 계란으로
+
+		// 현재 들고 있는 계란이 깨져있으면 다음 계란으로
 		if (eggs[idx].durability <= 0) {
-			dfs(idx+1);
+			dfs(idx + 1, brokenCount);
 			return;
 		}
-		
-		boolean canHit = false; // 때릴 수 있는 계란이 있는지 
-		
+
+		boolean hit = false; // 친 적이 있는지
+
 		for (int j = 0; j < N; j++) {
-			if (idx == j) continue; // 자기 계란 패스
-			if (eggs[j].durability <= 0) continue; // 깨진 계란 패스
-			
-			canHit = true;
-			
-			// idx 계란으로 j번째 계란 치기
+			if (idx == j)
+				continue;
+			if (eggs[j].durability <= 0)
+				continue;
+
+			hit = true;
+
+			// 현재 idx 계란으로 j 계란을 친다
 			eggs[idx].durability -= eggs[j].weight;
 			eggs[j].durability -= eggs[idx].weight;
-			
-			dfs(idx + 1);
-			
+
+			int newBroken = brokenCount;
+			if (eggs[idx].durability <= 0)
+				newBroken++;
+			if (eggs[j].durability <= 0)
+				newBroken++;
+
+			dfs(idx + 1, newBroken);
+
+			// 복구
 			eggs[idx].durability += eggs[j].weight;
 			eggs[j].durability += eggs[idx].weight;
 		}
-		
-		// 더 이상 칠 수 있는 계란이 하나도 없으면 그냥 다음 계란으로 계속 넘김
-		if (!canHit) {
-			dfs(idx + 1);
+
+		// 칠 수 있는 계란이 없다면 그냥 다음 계란
+		if (!hit) {
+			dfs(idx + 1, brokenCount);
 		}
 	}
-
 }
