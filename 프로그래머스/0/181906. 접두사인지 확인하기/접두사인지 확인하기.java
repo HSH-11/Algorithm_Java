82b088ecdb07d79b20ebaeee434e9c093d784720
HSH-11 <lyk1517@naver.com>
2025-11-12 15:18:54 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int n = is_prefix.length();
-        if (my_string.length() < n) return 0;
         
-        if (my_string.substring(0,n).equals(is_prefix)) return 1;
-        
-        return 0;
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
