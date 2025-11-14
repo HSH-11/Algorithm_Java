33ed4b5bd4858f21b2d4d7e84ef331cc0321fdce
HSH-11 <lyk1517@naver.com>
2025-11-15 01:56:16 +0900

    [level 0] Title: 접미사 배열, Time: 2.98 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String my_string) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 0; i < my_string.length(); i++) {
+            answer.add(my_string.substring(i));
+        }
+        System.out.println(answer);
+        Collections.sort(answer);
+        return answer;
+    }
+}
