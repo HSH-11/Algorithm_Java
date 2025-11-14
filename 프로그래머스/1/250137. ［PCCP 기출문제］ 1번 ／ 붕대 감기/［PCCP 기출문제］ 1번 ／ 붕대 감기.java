60a8fcf9c4744bfb07da10e25dfe358862ace183
HSH-11 <lyk1517@naver.com>
2025-11-15 05:30:30 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.10 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,40 +1,45 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int hp = health;
-        int t = bandage[0];
-        int x = bandage[1];
-        int y = bandage[2];
-        int time = 1;
-        int continuous = 0;
-        
+        int maxHealth = health;
+        int healInterval = bandage[0];
+        int basicHeal = bandage[1];
+        int bonusHeal = bandage[2];
+
+        int hp = maxHealth;
+        int combo = 0;
+        int currentTime = 0;
+
         for (int[] attack : attacks) {
             int attackTime = attack[0];
             int damage = attack[1];
-            
-            // 공격 전까지 회복
-            while (time < attackTime) {
-                if (hp < health) {
-                    hp = Math.min(health, hp + x);   
+
+            // 회복 가능 시간
+            int safeDuration = attackTime - currentTime - 1;
+
+            // 안전 시간 동안 회복 수행
+            for (int i = 0; i < safeDuration; i++) {
+                // 기본 회복
+                hp = Math.min(maxHealth, hp + basicHeal);
+                combo++;
+
+                // t초 연속 성공 시 추가 회복
+                if (combo == healInterval) {
+                    hp = Math.min(maxHealth, hp + bonusHeal);
+                    combo = 0;
                 }
-                
-                continuous++;
-                
-                // 붕대 완성 회복
-                if (continuous >= t) {
-                    hp = Math.min(health, hp + y);
-                    continuous = 0;
-                }
-                
-                time++;
-                  
             }
+
+            // 공격 처리
             hp -= damage;
             if (hp <= 0) return -1;
-            
-            continuous = 0;
-            time++;
-            
+
+            // 공격 받으면 연속 회복 초기화
+            combo = 0;
+
+            // 현재 시간 갱신
+            currentTime = attackTime;
         }
+
         return hp;
     }
 }
