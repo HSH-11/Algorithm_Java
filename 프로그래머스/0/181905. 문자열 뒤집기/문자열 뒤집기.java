03830f17a5b58fe5662edd15300c6a2cd7f842ef
HSH-11 <lyk1517@naver.com>
2025-11-16 23:24:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.75 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,10 @@
+import java.lang.StringBuilder;
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.substring(s, e + 1)).reverse();
+        sb.append(my_string.substring(e + 1));
+        String answer = my_string.substring(0, s) + sb.toString();
+        return answer;
+    }
+}
