7003b0a1c4276f3a3f9ad03e41b02da867e2e475
HSH-11 <lyk1517@naver.com>
2025-11-26 15:07:58 +0900

    [level 0] Title: rny_string, Time: 0.48 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        answer = rny_string.replaceAll("m","rn");
+        return answer;
+    }
+}
