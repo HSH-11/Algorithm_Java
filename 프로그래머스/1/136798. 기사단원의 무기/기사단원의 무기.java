c0bd4d4cf48616cd027786e82cce1fd774159013
HSH-11 <lyk1517@naver.com>
2026-01-02 13:19:38 +0900

    [level 1] Title: 기사단원의 무기, Time: 12.27 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] count = new int[number + 1];
+        
+        // 배수 원리를 이용한 약수 개수 계산
+        for (int i = 1; i <= number; i++ ) {
+            for (int j = i; j <= number; j += i) {
+                count[j]++;
+            }
+        }
+        
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
+        return answer;
+    }
+}
