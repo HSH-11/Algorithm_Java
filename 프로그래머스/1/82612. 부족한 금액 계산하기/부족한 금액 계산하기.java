326b2e3f381e7224703e4b03bd5f1d5205b3913d
HSH-11 <lyk1517@naver.com>
2026-01-05 14:25:01 +0900

    Refactor cost calculation to use arithmetic formula
    Optimize total cost calculation using formula.

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public long solution(int price, int money, int count) {
         long totalCost = 0;
-        
-        for (int i = 1; i <= count; i++) {
-            totalCost += (long) price * i;
-        }
-        
+           
+        totalCost = (long)price * count * (count + 1) / 2;
+                
         return money < totalCost ? totalCost - money : 0;
     }
 }
