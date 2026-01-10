fa43303561d638e02b1db807df4ffbe10b59348e
HSH-11 <lyk1517@naver.com>
2026-01-10 15:19:44 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 176 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        String[] files = new String[N];
+        
+        for (int i = 0; i < N; i++) {
+            files[i] = sc.next();
+        }
+        
+        StringBuilder pattern = new StringBuilder(files[0]);
+        int length = pattern.length();
+        
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < length; j++) {
+                if (pattern.charAt(j) != files[i].charAt(j)) {
+                    pattern.setCharAt(j, '?');
+                }
+            }
+        }
+        
+        System.out.println(pattern.toString());
+    }
+}
