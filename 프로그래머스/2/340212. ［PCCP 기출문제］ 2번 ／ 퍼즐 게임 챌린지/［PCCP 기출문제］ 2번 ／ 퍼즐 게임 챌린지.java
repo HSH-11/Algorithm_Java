8bce0c227c42d3f7917bd366c08c1f4789d0da34
HSH-11 <lyk1517@naver.com>
2026-01-12 15:59:48 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 44.86 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,38 @@
+// 숙련도가 오를수록 소요 시간은 줄어들음 -> 이분탐색
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int low = 1;
+        int high = 100000;
+        int answer = high;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (isPossible(diffs, times, limit, mid)) {
+                answer = mid; // 가능하면 숙련도 낮추기
+                high = mid - 1;
+            }else {
+                low = mid + 1; // 불가능하면 숙련도 높이기
+            }
+        }
+        
+        return answer;
+    }
+    
+    private boolean isPossible(int[] diffs, int[] times, long limit, int level) {
+        long total = 0;
+        for (int i = 0; i < diffs.length; i++) {
+            if (diffs[i] <= level) {
+                total += times[i];
+            } else {
+                long diffCount = diffs[i] - level;
+                
+                long prevTime = (i > 0) ? times[i-1] : 0;
+                total += diffCount * (times[i] + prevTime) + times[i];
+            }
+            
+            if (total > limit) return false; // 중간에 이미 넘어가면 바로 실패 반환
+        }
+        return true;
+    }
+}
