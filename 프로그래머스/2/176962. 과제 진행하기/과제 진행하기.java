32be836bf123b64bf9a2409619f4d9671bb504ed
HSH-11 <lyk1517@naver.com>
2026-01-17 05:05:42 +0900

    [level 2] Title: 과제 진행하기, Time: 7.82 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class Solution {
+    
+    class Task {
+        String name;
+        int start;
+        int remaining;
+        
+        public Task (String name, int start, int remaining) {
+            this.name = name;
+            this.start = start;
+            this.remaining = remaining;
+        }
+    }
+      
+    public String[] solution(String[][] plans) {
+        List<Task> tasks = new ArrayList<>();
+        for (String[] plan : plans) {
+            String[] time = plan[1].split(":");
+            int start = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+            int remaining = Integer.parseInt(plan[2]);
+            tasks.add(new Task(plan[0], start, remaining));
+        }
+        
+        // 시작 시간 순으로 정렬
+        Collections.sort(tasks, (a,b) -> a.start - b.start);
+        
+        Stack<Task> stopStack = new Stack<>();
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < tasks.size() - 1; i++) {
+            Task current = tasks.get(i);
+            Task next = tasks.get(i + 1);
+            
+            int nowToEnd = current.start + current.remaining;
+            
+            // 현재 과제를 끝내도 다음 과제 시작까지 시간이 남음
+            if (nowToEnd <= next.start) {
+                result.add(current.name);
+                int idleTime = next.start - nowToEnd;
+                
+                // 남은 시간 동안 스택에 있는 과제 해결
+                while (!stopStack.isEmpty() && idleTime > 0) {
+                    Task stopped = stopStack.peek();
+                    if (stopped.remaining <= idleTime) {
+                        idleTime -= stopped.remaining;
+                        result.add(stopStack.pop().name);
+                    } else {
+                        stopped.remaining -= idleTime;
+                        idleTime = 0;
+                    }
+                }
+            }
+            
+            // 현재 과제를 끝내지 못하고 다음 과제 시작
+            else {
+                current.remaining -= (next.start - current.start);
+                stopStack.push(current);
+            }        
+        }
+        
+        // 마지막 과제 처리
+        result.add(tasks.get(tasks.size() - 1).name);
+        
+        // 스택에 남은 과제 처리
+        while (!stopStack.isEmpty()) {
+            result.add(stopStack.pop().name);
+        }
+        
+        
+        return result.toArray(new String[0]);
+    }
+    
+    
+    
+    
+}
