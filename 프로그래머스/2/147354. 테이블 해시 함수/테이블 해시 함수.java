45b0f016785085e422a8d3d4a416afb3766065f6
HSH-11 <lyk1517@naver.com>
2026-01-22 04:06:27 +0900

    [level 2] Title: 테이블 해시 함수, Time: 10.35 ms, Memory: 145 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+        int n = data[0].length;
+        
+        Arrays.sort(data, (a,b) -> {
+            if (a[col-1] == b[col-1]) {
+                return b[0] - a[0];
+            }
+            return a[col-1] - b[col-1];
+        });
+        
+       
+        for (int i = row_begin; i <= row_end; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += data[i-1][j] % i;
+            }
+            answer = answer ^ sum;
+        }
+        return answer;
+    }
+}
