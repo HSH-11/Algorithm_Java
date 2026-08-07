461bf0453b2c299f165e412c9286a70947b05ecd
HSH-11 <lyk1517@naver.com>
2026-08-07 11:28:29 +0900

    [level 2] Title: 더 맵게, Time: 672.67 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        int answer = 0;
+        
+        while (pq.peek() < K) {
+            
+            if (pq.size() == 1) return -1;
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            pq.offer(first + second * 2);
+            
+            answer++;
+            
+        }
+        
+        return answer;
+    }
+}
